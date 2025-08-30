ea7e7f56b43a493f7d85cc3147f0a93717bf5dd8
marcie <kangsorom@gmail.com>
2025-08-30 14:42:46 +1000

    300825

---
+++ day5/Day5.py
@@ -0,0 +1,20 @@
+# student_scores = [150, 142, 185, 120, 184, 111, 1000, 23, 245, 234, 255, 2330, 300]
+# 
+# max_score = 0
+# 
+# for score in student_scores:
+#     if max_score < score:
+#         max_score = score
+#     else:
+#         continue
+# 
+# print(max_score)
+
+# Range Function with For Loop
+
+sum = 0
+
+for number in range(1, 101):
+    sum += number
+
+print(sum)
