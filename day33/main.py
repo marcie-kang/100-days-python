a4224d0ec0bbd8de87169f876db9582587b05722
marcie <kangsorom@gmail.com>
2025-10-04 13:19:05 +1000

    041025

---
+++ day33/main.py
@@ -0,0 +1,31 @@
+import requests
+from datetime import datetime
+
+MY_LAT = -27.46794
+MY_LONG = 153.02809
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# data = response.json()["iss_position"]
+# longitude = response.json()["iss_position"]["longitude"]
+# latitude = response.json()["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted":0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise, sunset)
+time_now = datetime.now()
+print(time_now.hour)
